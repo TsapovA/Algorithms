529ef1b8f037845119db7c384a5e4e852751950a
TsapovAndrei <Landcate@mail.ru>
2021-02-28 12:26:25 +0300

    add numbers/ThreeSumClosest algorithm

---
+++ src/alg/numbers/ThreeSumClosest.java
@@ -0,0 +1,51 @@
+package alg.numbers;
+
+import java.util.Arrays;
+
+/**
+ * Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target.
+ * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+ *
+ * Example 1:
+ *
+ * Input: nums = [-1,2,1,-4], target = 1
+ * Output: 2
+ * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ *
+ *
+ * Constraints:
+ *
+ * 3 <= nums.length <= 10^3
+ * -10^3 <= nums[i] <= 10^3
+ * -10^4 <= target <= 10^4
+ */
+public class ThreeSumClosest {
+
+    public static void main(String[] args) {
+        System.out.println(threeSumClosest(new int[] {-1, 2, 1, -4}, 1));
+    }
+
+    private static int threeSumClosest(int[] nums, int target) {
+        int diff = Integer.MAX_VALUE;
+        int length = nums.length;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < length && diff != 0; ++i) {
+            int low = i + 1;
+            int high = length - 1;
+
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (Math.abs(sum - target) < Math.abs(diff)) {
+                    diff = sum - target;
+                }
+                if (sum < target) {
+                    ++low;
+                } else {
+                    --high;
+                }
+            }
+        }
+        return target + diff;
+    }
+}
