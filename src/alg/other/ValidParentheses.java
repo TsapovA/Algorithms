692f61c6073c224ea685d1d6f70672c1a6ff4623
TsapovAndrei <Landcate@mail.ru>
2021-03-14 19:10:47 +0300

    others/validParentheses

---
+++ src/alg/other/ValidParentheses.java
@@ -0,0 +1,86 @@
+package alg.other;
+
+import java.util.Stack;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "()"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "()[]{}"
+ * Output: true
+ * Example 3:
+ *
+ * Input: s = "(]"
+ * Output: false
+ * Example 4:
+ *
+ * Input: s = "([)]"
+ * Output: false
+ * Example 5:
+ *
+ * Input: s = "{[]}"
+ * Output: true
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'.
+ */
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+        System.out.println(isValid("([)]"));
+        System.out.println(isValid("{[]}"));
+    }
+
+    private static boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : chars) {
+            switch (ch) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(ch);
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                    if (stack.size() == 0) {
+                        return false;
+                    }
+
+                    char pop = stack.pop();
+                    if (ch == ')' && pop != '(') {
+                        return false;
+                    }
+                    if (ch == ']' && pop != '[') {
+                        return false;
+                    }
+                    if (ch == '}' && pop != '{') {
+                        return false;
+                    }
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
