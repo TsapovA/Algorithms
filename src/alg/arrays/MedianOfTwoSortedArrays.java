f2422ed36bfeae5795ab1933fcdaf40e5682fe54
TsapovAndrei <Landcate@mail.ru>
2021-06-01 14:09:42 +0300

    arrays/findMedianSortedArrays

---
+++ src/alg/arrays/MedianOfTwoSortedArrays.java
@@ -0,0 +1,85 @@
+package alg.arrays;
+
+/**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+ *
+ * The overall run time complexity should be O(log (m+n)).
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [1,3], nums2 = [2]
+ * Output: 2.00000
+ * Explanation: merged array = [1,2,3] and median is 2.
+ * Example 2:
+ *
+ * Input: nums1 = [1,2], nums2 = [3,4]
+ * Output: 2.50000
+ * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ * Example 3:
+ *
+ * Input: nums1 = [0,0], nums2 = [0,0]
+ * Output: 0.00000
+ * Example 4:
+ *
+ * Input: nums1 = [], nums2 = [1]
+ * Output: 1.00000
+ * Example 5:
+ *
+ * Input: nums1 = [2], nums2 = []
+ * Output: 2.00000
+ *
+ *
+ * Constraints:
+ *
+ * nums1.length == m
+ * nums2.length == n
+ * 0 <= m <= 1000
+ * 0 <= n <= 1000
+ * 1 <= m + n <= 2000
+ * -10^6 <= nums1[i], nums2[i] <= 10^6
+ */
+public class MedianOfTwoSortedArrays {
+
+    public static void main(String[] args) {
+        System.out.println(findMedianSortedArrays(new int[]{}, new int[]{2, 3}));
+        System.out.println(findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
+        System.out.println(findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println(findMedianSortedArrays(new int[]{0, 0}, new int[]{0, 0}));
+        System.out.println(findMedianSortedArrays(new int[]{}, new int[]{1}));
+        System.out.println(findMedianSortedArrays(new int[]{2}, new int[]{}));
+    }
+
+    private static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int x = nums1.length;
+        int y = nums2.length;
+
+        int low = 0;
+        int high = x;
+
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxLeftX = partitionX > 0 ? nums1[partitionX - 1] : Integer.MIN_VALUE;
+            int minRightX = partitionX < x ? nums1[partitionX] : Integer.MAX_VALUE;
+
+            int maxLeftY = partitionY > 0 ? nums2[partitionY - 1] : Integer.MIN_VALUE;
+            int minRightY = partitionY < y ? nums2[partitionY] : Integer.MAX_VALUE;
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                return ((x + y) % 2 == 0) ?
+                    (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0 :
+                    Math.max(maxLeftX, maxLeftY);
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+        throw new IllegalArgumentException("Shouldn't be here");
+    }
+}
