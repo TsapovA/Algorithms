1e41bc5039e87bfb6bc9a7e835e96e39f3ef9fbd
Andyup <Landcate@mail.ru>
2022-11-22 11:58:48 +0300

    infix-to-postfix-ver2

--- src/alg/stack/InfixToPostfix.java
+++ src/alg/stack/InfixToPostfix.java
@@ -14,51 +14,129 @@ public class InfixToPostfix {
     public static void main(String[] args) {
         String exp = "a+b*(c^d-e)^(f+g*h)-i";
         System.out.println(infixToPostfix(exp));
+        System.out.println(infixToPostfix2(exp));
     }
 
     private static String infixToPostfix(String exp) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         Stack<Character> stack = new Stack<>();
 
         char[] chars = exp.toCharArray();
         for (char ch: chars) {
             if (Character.isLetterOrDigit(ch)) {
-                result += ch;
+                result.append(ch);
             } else if (ch == '(') {
                 stack.push(ch);
             } else if (ch == ')') {
                 while (!stack.isEmpty() && stack.peek() != '(') {
-                    result += stack.pop();
+                    result.append(stack.pop());
                 }
                 stack.pop();
             } else { // operand
                 while (!stack.isEmpty() && getPrec(ch) <= getPrec(stack.peek())) {
-                    result += stack.pop();
+                    result.append(stack.pop());
                 }
                 stack.push(ch);
             }
         }
 
         while (!stack.isEmpty()) {
-            result += stack.pop();
+            result.append(stack.pop());
         }
-        return result;
+        return result.toString();
     }
 
     // A utility function to return precedence of a given operator Higher returned value means higher precedence
     private static int getPrec(char ch) {
-        switch (ch) {
-            case '+':
-            case '-':
-                return 1;
+        return switch (ch) {
+            case '+', '-' -> 1;
+            case '*', '/' -> 2;
+            case '^' -> 3;
+            default -> -1;
+        };
+    }
 
-            case '*':
-            case '/':
-                return 2;
-
-            case '^':
-                return 3;
+    private static String infixToPostfix2(String str) {
+        StringBuilder resBuilder = new StringBuilder();
+        StackX<Character> stack = new StackX<>(str.length());
+        char[] chars = str.toCharArray();
+        for (char ch: chars) {
+            if (Character.isLetterOrDigit(ch)) {
+                resBuilder.append(ch);
+                continue;
+            }
+            switch (ch) {
+                case '(' -> stack.push(ch);
+                case ')' -> extractOp(stack, resBuilder);
+                default -> operator(stack, resBuilder, ch);
+            }
         }
-        return -1;
+        while (!stack.isEmpty()) {
+            resBuilder.append(stack.pop());
+        }
+        return resBuilder.toString();
+    }
+
+    private static void operator(StackX<Character> stack, StringBuilder resBuilder, char ch) {
+        int priority = getPriority(ch);
+        while (!stack.isEmpty()) {
+            char prev = stack.pop();
+            if (prev == '(') {
+                stack.push(prev);
+                break;
+            } else if (getPriority(prev) < priority) {
+                stack.push(prev);
+                break;
+            } else {
+                resBuilder.append(prev);
+            }
+        }
+        stack.push(ch);
+    }
+
+    private static void extractOp(StackX<Character> stack, StringBuilder resBuilder) {
+        while (!stack.isEmpty()) {
+            Character prev = stack.pop();
+            if (prev == '(') {
+                return;
+            }
+            resBuilder.append(prev);
+        }
+    }
+
+    private static int getPriority(char ch) {
+        return switch(ch) {
+            case '+', '-' -> 1;
+            case '*', '/' -> 2;
+            case '^' -> 3;
+            default -> throw new IllegalArgumentException("char=" + ch);
+        };
+    }
+
+    private static class StackX<T> {
+
+        private final int size;
+        private final Object[] array;
+        private int top = -1;
+
+        public StackX(int size) {
+            this.size = size;
+            this.array = new Object[size];
+        }
+
+        public void push(T t) {
+            array[++top] = t;
+        }
+
+        public T pop() {
+            Object res = array[top];
+            array[top] = null;
+            --top;
+            return (T) res;
+        }
+
+        public boolean isEmpty() {
+            return top == -1;
+        }
     }
 }
